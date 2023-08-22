ca88c6b98ec7e9439a6efe6fd448e9d23c19f327
Noah <noah.ntabo@student.moringaschool.com>
2023-08-23 00:27:09 +0300

    Completed assignment

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,24 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+
+    def add_item(self, title, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([title] * quantity)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total *= (100 - self.discount) / 100
+            print(f"After the discount, the total comes to ${self.total:.2f}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        if self.items:
+            last_item_price = self._get_item_price(self.items.pop())
+            self.total -= last_item_price
+
+    def _get_item_price(self, title):
+        return 0.0  # Replace with logic to get item price based on title
